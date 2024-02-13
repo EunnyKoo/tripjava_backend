d733be87fe1422f219f61fbf24ef337212dc0f3a
joojooring <ghkdwnfl0557@naver.com>
2024-02-13 19:10:20 +0900

    [ADD] - PACKAGE CREATE

---
+++ trip-java/src/test/java/com/trip/tripjava/TripJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.tripjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
