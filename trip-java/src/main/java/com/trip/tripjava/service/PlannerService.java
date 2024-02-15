557c21273afb109c4e97e6b468a41ebd0649ffa6
joojooring <ghkdwnfl0557@naver.com>
2024-02-15 22:18:51 +0900

    deleteDate

--- trip-java/src/main/java/com/trip/tripjava/service/PlannerService.java
+++ trip-java/src/main/java/com/trip/tripjava/service/PlannerService.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
 @Builder
 @Service
 public class PlannerService {
@@ -26,11 +28,16 @@ public void selectDate(PlannerDTO plannerDTO) {
 
         PlannerEntity plannerEntity = PlannerEntity.builder()
                 .planner_startday(plannerDTO.getPlanner_startday())
-                .planner_endday(plannerDTO.getPlanner_endday())
+                .planner_startday(plannerDTO.getPlanner_endday())
 //                .planner_region(plannerDTO.getPlanner_region())
 //                .user(userEntity.getId())
                 .build();
 
         plannerRepository.save(plannerEntity);
     }
+    public void deleteDate(long planner_no) {
+        PlannerEntity plannerEntity = plannerRepository.findById(String.valueOf(planner_no))
+                .orElseThrow(()->new NoSuchElementException("plannerdate patch : planner_no is wrong"));
+        plannerRepository.delete(plannerEntity);
+        }
 }
