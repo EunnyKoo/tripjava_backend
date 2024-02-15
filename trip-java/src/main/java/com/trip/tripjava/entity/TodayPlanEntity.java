abc017f87820b621a5e4cb805f33dfd26a3fccb3
seeun <jungseeun0310@naver.com>
2024-02-15 15:52:32 +0900

    fix : Entity 내부 컬럼명 수정

--- trip-java/src/main/java/com/trip/tripjava/entity/TodayPlanEntity.java
+++ trip-java/src/main/java/com/trip/tripjava/entity/TodayPlanEntity.java
@@ -12,22 +12,19 @@ import lombok.NoArgsConstructor;
 @Table(name = "today_plan")
 @NoArgsConstructor
 @AllArgsConstructor
-//@ManyToOne
-//@JoinColumn
 public class TodayPlanEntity {
 
     @Id
     @Column(name = "today_no", nullable = false)
     private int today_no;
 
-    // AttractionEntity, TripEntity 만들어지면 주석 해제
-//    @ManyToOne
-//    @JoinColumn(name = "attraction_no", referencedColumnName = "attraction_no")
-//    private AttractionEntity attraction_no;
+    @ManyToOne
+    @JoinColumn(name = "contentid", referencedColumnName = "contentid")
+    private TouristEntity contentid;
 
-//    @ManyToOne
-//    @JoinColumn(name = "planner_no", referencedColumnName = "planner_no")
-//    private TripEntity planner_no;
+    @ManyToOne
+    @JoinColumn(name = "planner_no", referencedColumnName = "planner_no")
+    private PlannerEntity planner_no;
 
     @Column(name = "today_date", nullable = false)
     private String today_date;
