2824939b9702c825f21d05c4658cfeef6f617a08
EunnyKoo <kooeun55555@gmail.com>
2024-02-15 15:04:22 +0900

    update: TouristEntity 추가

---
+++ trip-java/src/main/java/com/trip/tripjava/entity/TouristEntity.java
@@ -0,0 +1,60 @@
+package com.trip.tripjava.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="tourist")
+public class TouristEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "contentid")
+    private long contentid;
+
+    @Column(name = "contenttypeid")
+    private int contenttypeid;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "addr1")
+    private String addr1;
+
+    @Column(name = "areacode")
+    private String areacode;
+
+    @Column(name = "sigungucode")
+    private String sigungucode;
+
+    @Column(name = "firstimage")
+    private String firstimage;
+
+    @Column(name = "overview")
+    private String overview;
+
+    @Column(name = "mapx")
+    private String mapx;
+
+    @Column(name = "mapy")
+    private String mapy;
+
+    /*
+    @Column(name = "cat1")
+    private String cat1;
+
+    @Column(name = "cat2")
+    private String cat2;
+
+    @Column(name = "cat3")
+    private String cat3;
+    */
+
+
+}
