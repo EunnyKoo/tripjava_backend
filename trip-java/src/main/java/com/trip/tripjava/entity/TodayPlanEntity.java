dc57fe09efbaef7314c5a98e6baad4b858ce6c32
seeun <jungseeun0310@naver.com>
2024-02-15 14:14:42 +0900

    fix : today_plan 오류 수정

--- trip-java/src/main/java/com/trip/tripjava/entity/TodayPlanEntity.java
+++ trip-java/src/main/java/com/trip/tripjava/entity/TodayPlanEntity.java
@@ -1,9 +1,6 @@
 package com.trip.tripjava.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,19 +12,22 @@ import lombok.NoArgsConstructor;
 @Table(name = "today_plan")
 @NoArgsConstructor
 @AllArgsConstructor
+//@ManyToOne
+//@JoinColumn
 public class TodayPlanEntity {
 
     @Id
     @Column(name = "today_no", nullable = false)
     private int today_no;
 
-    @ManyToOne
-    @JoinColumn(name = "attraction_no", referencedColumnName = "attraction_no")
-    private AttractionEntity attraction_no;
+    // AttractionEntity, TripEntity 만들어지면 주석 해제
+//    @ManyToOne
+//    @JoinColumn(name = "attraction_no", referencedColumnName = "attraction_no")
+//    private AttractionEntity attraction_no;
 
-    @ManyToOne
-    @JoinColumn(name = "planner_no", referencedColumnName = "planner_no")
-    private TripEntity planner_no;
+//    @ManyToOne
+//    @JoinColumn(name = "planner_no", referencedColumnName = "planner_no")
+//    private TripEntity planner_no;
 
     @Column(name = "today_date", nullable = false)
     private String today_date;
