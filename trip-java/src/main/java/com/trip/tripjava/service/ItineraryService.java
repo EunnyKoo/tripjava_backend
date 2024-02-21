99e85dfcd4e69554dba367254b593a9d0e226f46
EunnyKoo <kooeun55555@gmail.com>
2024-02-21 17:22:10 +0900

    itinerary add

---
+++ trip-java/src/main/java/com/trip/tripjava/service/ItineraryService.java
@@ -0,0 +1,8 @@
+package com.trip.tripjava.service;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItineraryService {
+}
