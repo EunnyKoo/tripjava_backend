e4e52849e177e36145b800ce2a146743af59d64f
seeun <jungseeun0310@naver.com>
2024-02-26 15:15:15 +0900

    fix : 체크리스트 planner_no 저장되게 수정

--- trip-java/src/main/java/com/trip/tripjava/repository/ChecklistRepository.java
+++ trip-java/src/main/java/com/trip/tripjava/repository/ChecklistRepository.java
@@ -2,6 +2,11 @@ package com.trip.tripjava.repository;
 
 import com.trip.tripjava.entity.ChecklistEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChecklistRepository extends JpaRepository<ChecklistEntity, Long> {
+    @Query("select c from ChecklistEntity c where c.planner.planner_no = :planner_no")
+    List<ChecklistEntity> findByPlanner_Planner_no(long planner_no);
 }
